90dd28de7efad57a300b97881bc018c6e71190b8
Ehud Ben-Reuven <udi@benreuven.com>
2019-02-04 18:39:49 -0500

    .

---
+++ README.md
@@ -0,0 +1,16 @@
+# How BERT works
+https://medium.com/@mromerocalvo/dissecting-bert-part1-6dcf5360b07f
+https://medium.com/dissecting-bert/dissecting-bert-part2-335ff2ed9c73
+
+# Code
+## torch
+https://github.com/huggingface/pytorch-pretrained-BERT
+
+# Example
+https://towardsdatascience.com/a-i-plays-mad-libs-and-the-results-are-terrifying-78fa44e7f04e
+
+```
+pip install torch
+pip install pytorch-pretrained-bert
+python predict_missing.py
+```
---
+++ predict_missing.py
@@ -0,0 +1,16 @@
+# How BERT works
+https://medium.com/@mromerocalvo/dissecting-bert-part1-6dcf5360b07f
+https://medium.com/dissecting-bert/dissecting-bert-part2-335ff2ed9c73
+
+# Implementation
+## torch
+https://github.com/huggingface/pytorch-pretrained-BERT#notebooks
+
+# Demo
+https://towardsdatascience.com/a-i-plays-mad-libs-and-the-results-are-terrifying-78fa44e7f04e
+```
+pip install torch
+pip install pytorch-pretrained-bert
+python predict_missing.py # https://gist.github.com/jessevig/6394759f263d0839839c9fb785f3d3b4
+```
+
